a2c55f31383e17d260476d8f6bfac633bef15d47
rishu <rishabhpawar401@gmail.com>
2025-11-13 23:35:08 +0530

    Handling url

--- Backend-node/SERVER/index.js
+++ Backend-node/SERVER/index.js
@@ -1,15 +1,23 @@
 const http = require("http");
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req,res)=>{
+  if(req.url === '/favicon.ico') return res.end();
   const log = `${Date.now()}: ${req.url} New req recived \n `;
+  const myUrl = url.parse(req.url,true);
+  console.log(myUrl)
   fs.appendFile('log.txt',log, (err,data)=> {
-    switch(req.url){
-      case '/': res.end("homepage");
+    switch(myUrl.pathname){
+      case '/': 
+      res.end("homepage");
       break;
-      case '/about': res.end("I am rishabh ");
+      case '/about':
+      const username = myUrl.query.myname
+      res.end(`I am ${username}`);
       break;
-      default: res.end("404 error")
+      default: 
+      res.end("404 error")
     }
   });
   
