76a299c4e63d30cba24b20ef3681a5bdfa972cd7
rishu <rishabhpawar401@gmail.com>
2025-11-14 23:38:22 +0530

    HTTP-methods

--- Backend-node/SERVER/index.js
+++ Backend-node/SERVER/index.js
@@ -4,18 +4,21 @@ const url = require("url")
 
 const myServer = http.createServer((req,res)=>{
   if(req.url === '/favicon.ico') return res.end();
-  const log = `${Date.now()}: ${req.url} New req recived \n `;
+  const log = `${Date.now()}: ${req.method} ${req.url} New req recived \n `;
   const myUrl = url.parse(req.url,true);
   console.log(myUrl)
   fs.appendFile('log.txt',log, (err,data)=> {
     switch(myUrl.pathname){
       case '/': 
-      res.end("homepage");
+      if(req.method === 'GET') res.end("Youre at home page")
       break;
       case '/about':
       const username = myUrl.query.myname
       res.end(`I am ${username}`);
       break;
+      case'/signup':
+      if(req.method === 'GET') res.end("This is signup form")
+      else if(req.method ==="POST") res.end ("Success")
       default: 
       res.end("404 error")
     }
