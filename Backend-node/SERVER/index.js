0bb3f0b755eb59d884adf6d133e5a789a06a2ede
rishu <rishabhpawar401@gmail.com>
2025-11-16 23:08:55 +0530

    Start with express

--- Backend-node/SERVER/index.js
+++ Backend-node/SERVER/index.js
@@ -1,8 +1,20 @@
 const http = require("http");
 const fs = require("fs")
 const url = require("url")
+const express = require("express");
 
-const myServer = http.createServer((req,res)=>{
+const app = express();
+
+app.get("/",(req,res)=>{
+  return res.send("Hello from home page exp")
+});
+
+app.get("/about",(req,res)=>{
+  return res.send(`Hello you are ${req.query.name} and you are ${req.query.age}`)
+})
+ 
+
+function myHandler (req,res){
   if(req.url === '/favicon.ico') return res.end();
   const log = `${Date.now()}: ${req.method} ${req.url} New req recived \n `;
   const myUrl = url.parse(req.url,true);
@@ -24,6 +36,11 @@ const myServer = http.createServer((req,res)=>{
     }
   });
   
-});
+}
 
-myServer.listen(8000, ()=>console.log("server started"));
+
+app.listen(8000,()=> console.log("Server Start"))
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, ()=>console.log("server started"));
