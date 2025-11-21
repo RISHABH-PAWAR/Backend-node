72a4cf32fdde9fb4a51296720fee3cbac5080ff1
rishu <rishabhpawar401@gmail.com>
2025-11-21 23:54:40 +0530

    use ejs for server side rendering

--- Backend-node/02urlShortner/index.js
+++ Backend-node/02urlShortner/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path")
 const urlRoute = require("./routes/url")
 const URL = require("./models/url")
 const {connectTOMongoDB} = require("./connect")
@@ -10,7 +11,19 @@ const PORT = 8001;
 connectTOMongoDB("mongodb+srv://rishabhrajput245304_db_user:UI0W07V2qtgyQQoh@urlshortner.ubcmhqm.mongodb.net/?appName=urlShortner")
 .then(()=> console.log("MongoDB is connected"));
 
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
 app.use(express.json())
+
+app.get("/test" async(req,res)=>{
+  const allUrls = await URL.find({});
+  return res.render("home",{
+    urls: allUrls,
+    
+  })
+})
+
 app.get("/:shortId",async(req,res)=>{
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate({
